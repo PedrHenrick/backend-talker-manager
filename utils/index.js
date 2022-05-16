67fac135940378d9f7b8f62419c2ff1048c2257f
Pedro Henrick <phmc@ic.ufal.br>
2022-05-16 15:26:39 -0300

    Pasta utils e arquivo com funções auxialiadoras

---
+++ utils/index.js
@@ -0,0 +1,16 @@
+const fs = require('fs/promises');
+
+const readFile = async () => {
+  const content = await fs.readFile('task.json', 'utf-8');
+
+  return JSON.parse(content);
+};
+
+const writeFile = async (content) => {
+  await fs.writeFile('task.json', JSON.stringify(content), 'utf-8');
+};
+
+module.exports = {
+  readFile,
+  writeFile,
+};
