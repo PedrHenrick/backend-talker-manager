256905103fc6cb989750c5b6ad4aedfad2f3747d
Pedro Henrick <phmc@ic.ufal.br>
2022-05-16 19:04:35 -0300

    Requisito 4: Validações no endpoint /login adicion

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const validateLogin = require('../middleware/validateLogin');
 const { generateToken } = require('../utils');
 
 const loginRoute = express.Router();
 
-loginRoute.post('/', async (_req, res) => {
+loginRoute.post('/', validateLogin, async (_req, res) => {
   // const { email, password } = req.body;
 
   const token = generateToken(8);
