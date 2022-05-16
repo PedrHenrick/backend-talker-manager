5246468e83ea0a6f491685502e9a59a85999a7b4
Pedro Henrick <phmc@ic.ufal.br>
2022-05-16 19:41:22 -0300

    adicionando validação do token a função

--- middleware/validateAuth.js
+++ middleware/validateAuth.js
@@ -1,11 +1,11 @@
-const validateAuth = (req, _res, next) => {
+const validateAuth = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization) {
-    next({
-      statusCode: 401,
-      message: 'Token não encontrado',
-    });
+    return res.status(401).json({ message: 'Token não encontrado' });
+  }
+  if (authorization.length < 16 || authorization.length > 16) {
+    return res.status(401).json({ message: 'Token inválido' });
   }
 
   next();
