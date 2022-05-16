a09d1a8f4acf31b6d9dc00649acac9522de124cd
Pedro Henrick <phmc@ic.ufal.br>
2022-05-16 19:52:17 -0300

    Refatorando lógica da validação de email

--- middleware/validateLogin.js
+++ middleware/validateLogin.js
@@ -1,15 +1,18 @@
 const joi = require('joi');
 
+const emailVerificationAndErrorMessages = {
+  'any.required': 'O campo "email" é obrigatório',
+  'string.email': 'O "email" deve ter o formato "email@email.com"',
+};
+const passwordVerificationAndErrorMessages = {
+  'any.required': 'O campo "password" é obrigatório',
+  'string.min': 'O "password" deve ter pelo menos 6 caracteres',
+};
+
 const validateLogin = (req, res, next) => {
   const schema = joi.object({
-    email: joi.string().email().required().messages({
-      'any.required': 'O campo "email" é obrigatório',
-      'string.email': 'O "email" deve ter o formato "email@email.com"',
-    }),
-    password: joi.string().min(6).required().messages({
-      'any.required': 'O campo "password" é obrigatório',
-      'string.min': 'O "password" deve ter pelo menos 6 caracteres',
-    }),
+    email: joi.string().email().required().messages(emailVerificationAndErrorMessages),
+    password: joi.string().min(6).required().messages(passwordVerificationAndErrorMessages),
   });
 
   const { error } = schema.validate(req.body);
