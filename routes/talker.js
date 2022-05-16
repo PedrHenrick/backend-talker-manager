0e33a60b3a090ad8766cdd3817f4e02fb5126990
Pedro Henrick <phmc@ic.ufal.br>
2022-05-16 16:15:13 -0300

    Requisito 2: Criando get para /talker/:id e manipulando erro

--- routes/talker.js
+++ routes/talker.js
@@ -12,6 +12,10 @@ talkerRoute.get('/:id', async (req, res) => {
   const talkers = await readFile();
 
   const searchedTalker = talkers.find((talker) => talker.id === Number(id));
+  
+  if (!searchedTalker) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
 
   return res.status(200).json(searchedTalker);
 });
