d83edb5f7d12b3dbc47b0dcf2a8e3ad477b0453f
Pedro Henrick <phmc@ic.ufal.br>
2022-05-16 19:25:38 -0300

    Adicionando método post no endpoint /talker

--- routes/talker.js
+++ routes/talker.js
@@ -1,10 +1,27 @@
 const express = require('express');
-const { readFile } = require('../utils/index');
+const { readFile, writeFile } = require('../utils/index');
 
 const talkerRoute = express.Router();
 
 talkerRoute.get('/', async (_req, res) => {
   res.status(200).json(await readFile());
+});
+
+talkerRoute.post('/', async (req, res) => {
+  const { name, age, talk } = req.body;
+  const talkers = await readFile();
+
+  const newTalker = {
+    name,
+    age,
+    id: Math.max(...talkers.map((talker) => talker.id)) + 1,
+    talk,
+  };
+
+  talkers.push(newTalker);
+  await writeFile(talkers);
+
+  res.status(200).json({ message: 'Insert OK' });
 });
 
 talkerRoute.get('/:id', async (req, res) => {
