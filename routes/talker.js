86b9364a38a9abb9564174cbb3c6150328961004
Pedro Henrick <phmc@ic.ufal.br>
2022-05-16 15:45:08 -0300

    Exportando rota talker e arrumando erro na rota

--- routes/talker.js
+++ routes/talker.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const readFile = require('../utils/index');
+const { readFile } = require('../utils/index');
 
 const talker = express.Router();
 
 talker.get('/', async (_req, res) => {
   res.status(200).send(await readFile());
 });
+
+module.exports = talker;
