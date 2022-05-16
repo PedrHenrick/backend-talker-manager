18b6b296d8925b23897e69d675b10a494632b7a2
Pedro Henrick <phmc@ic.ufal.br>
2022-05-16 16:14:23 -0300

    Adicionando get para o /talker/:id

--- routes/talker.js
+++ routes/talker.js
@@ -7,4 +7,13 @@ talkerRoute.get('/', async (_req, res) => {
   res.status(200).json(await readFile());
 });
 
+talkerRoute.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkers = await readFile();
+
+  const searchedTalker = talkers.find((talker) => talker.id === Number(id));
+
+  return res.status(200).json(searchedTalker);
+});
+
 module.exports = talkerRoute;
