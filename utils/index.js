39383b5ac795071d5691280810dc59d2b2950497
Pedro Henrick <phmc@ic.ufal.br>
2022-05-16 17:32:15 -0300

    Adicionando função de geração de token

--- utils/index.js
+++ utils/index.js
@@ -1,4 +1,5 @@
 const fs = require('fs/promises');
+const crypto = require('crypto');
 
 const readFile = async () => {
   const content = await fs.readFile('talker.json', 'utf-8');
@@ -10,7 +11,12 @@ const writeFile = async (content) => {
   await fs.writeFile('task.json', JSON.stringify(content), 'utf-8');
 };
 
+const generateToken = (bites) => crypto.randomBytes(bites).toString('hex');
+
+module.exports = generateToken;
+
 module.exports = {
   readFile,
   writeFile,
+  generateToken,
 };
