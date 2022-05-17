83c8b1e8aef035190f80e137ea526a969a7a5fce
Pedro Henrick <phmc@ic.ufal.br>
2022-05-17 09:38:47 -0300

    Requisito 6: Endpoint PUT /talker/:id criado

--- routes/talker.js
+++ routes/talker.js
@@ -39,4 +39,21 @@ talkerRoute.get('/:id', async (req, res) => {
   return res.status(200).json(searchedTalker);
 });
 
+talkerRoute.put('/:id', validateAuth, validateTalkers, async (req, res) => {
+  const { id } = req.params;
+  const { name, age, talk } = req.body;
+  const talkers = await readFile();
+
+  const searchedTalker = talkers.find((talker) => talker.id === Number(id));
+  if (!searchedTalker) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+
+  const attTalker = { ...searchedTalker, name, age, talk };
+  talkers.splice(talkers.indexOf(searchedTalker), 1, attTalker);
+
+  await writeFile(talkers);
+  res.status(200).json(attTalker);
+});
+
 module.exports = talkerRoute;
